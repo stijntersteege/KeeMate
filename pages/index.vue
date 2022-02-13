3eac265cc5b6b2ac7945d977ed6c4c9bf16c2c1e
Stijn ter Steege <mail@stijntersteege.com>
2022-02-13 21:57:06 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -402,7 +402,7 @@ export default {
         passwordLength = 64;
       }
 
-      for (var i = 0; i <= passwordLength; i++) {
+      for (var i = 0; i < passwordLength; i++) {
         var randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
       }
